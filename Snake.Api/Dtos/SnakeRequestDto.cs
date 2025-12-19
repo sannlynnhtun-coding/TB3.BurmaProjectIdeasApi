8114c497d918aa94a9316d75292d9121405c0bad
nyanlinhtetdev <nlinhtet.dev22@gmail.com>
2025-12-19 21:37:18 +0800

    feat: create Dtos for snake API project

---
+++ Snake.Api/Dtos/SnakeRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Snake.Api.Dtos
+{
+    public class SnakeRequestDto
+    {
+        public string? MMName { get; set; }
+        public string? EngName { get; set; }
+        public string? Detail { get; set; }
+        public string? IsPoison { get; set; }
+        public string? IsDanger { get; set; }
+    }
+}
